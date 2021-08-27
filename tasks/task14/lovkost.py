eef4b3f6a52780dae42e3273d4544f51bda51dde
Evgeniy Budaev <budaev.e@gmail.com>
2021-08-27 16:01:18 +0300

    Ловкость рук

--- tasks/task14/lovkost.py
+++ tasks/task14/lovkost.py
@@ -1,30 +1,34 @@
-# ID: 52424425
+# ID: 52461334
 
 from typing import List
 from collections import Counter
 
 
-def replace_dots_to_zero(word):
+def replace_dots_to_zero(word: str) -> str:
     """Заменяет в строке символы . на 0"""
     return word.replace('.', '0')
 
 
-def main():
-    """Бизнес-логика при запуске этого модуля как основного!"""
-    all_numbers: List[int] = []
-    coefficient = int(input())
-
-    for row in range(4):
-        all_numbers.extend(list(map(int, replace_dots_to_zero(input()))))
-
+def counter_scores(all_values: List[int], factor: int) -> int:
+    """Подсчет максимального кол-ва баллов."""
     new_dict = {
         key: value
-        for key, value in Counter(all_numbers).items()
-        if (coefficient * 2) >= value and key != 0
+        for key, value in Counter(all_values).items()
+        if (factor * 2) >= value and key != 0
     }
 
-    print(len(new_dict))
+    return len(new_dict)
+
+
+def main(all_values: List[int], factor: int) -> int:
+    """Бизнес-логика при запуске этого модуля как основного!"""
+    return counter_scores(all_values, factor)
 
 
 if __name__ == '__main__':
-    main()
+    all_numbers: List[int] = []
+    coefficient = int(input())
+    for row in range(4):
+        all_numbers.extend(list(map(int, replace_dots_to_zero(input()))))
+    result = main(all_numbers, coefficient)
+    print(result)
