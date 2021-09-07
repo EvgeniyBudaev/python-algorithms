5873babf65241e6b939d8958121d3e78f75869a1
Evgeniy Budaev <budaev.e@gmail.com>
2021-09-07 19:10:19 +0300

    Вставка, удаление в связанном списке

--- tasks/task16/code.py
+++ tasks/task16/code.py
@@ -6,9 +6,19 @@
 
 
 def solution(node, idx):
-    # Your code
-    # ヽ(´▽`)/
-    pass
+    if idx == 0:
+        head = node.next_item
+        return head
+    else:
+        head = node
+    while idx-1:
+        if node.next_item is not None:
+            node = node.next_item
+        else:
+            return head
+        idx -= 1
+    node.next_item = node.next_item.next_item
+    return head
 
 def test():
     node3 = Node("node3", None)
--- tasks/task16/nelyubimoeDelo.py
+++ tasks/task16/nelyubimoeDelo.py
@@ -21,9 +21,19 @@ class Node:
         return head
 
     def solution(node, idx):
-        while node:
-            print(node.value)
-            node = node.next_item
+        if idx == 0:
+            head = node.next_item
+            return head
+        else:
+            head = node
+        while idx - 1:
+            if node.next_item is not None:
+                node = node.next_item
+            else:
+                return head
+            idx -= 1
+        node.next_item = node.next_item.next_item
+        return head
 
 
 def test():
