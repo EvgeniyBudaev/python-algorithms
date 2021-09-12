153c2eb0dce6a8e4f4e566eae254e7027960b75b
Evgeniy Budaev <budaev.e@gmail.com>
2021-09-12 09:42:34 +0300

    Deque

--- yandex-python-algorithms/Sprint13/Deque/Deque.py
+++ yandex-python-algorithms/Sprint13/Deque/Deque.py
@@ -1,67 +1,60 @@
-# 52750674
+# 52964549
 class Deque:
     def __init__(self, n):
-        self.queue = [None] * n
-        self.max_size = n
-        self.head = 0
-        self.tail = 0
-        self.size = 0
+        self.__deque_list = [None] * n
+        self.__max_size = n
+        self.__front, self.__back, self.__size = 0, 0, 0
 
     def is_empty(self):
         """Проверить на пустоту."""
-        return self.size == 0
+        return self.__size == 0
+
+    def calc_index(self, var: int, val: int) -> int:
+        if val:
+            return (var + 1) % self.__max_size
+        else:
+            return (var - 1) % self.__max_size
 
     def overflow(self):
         """Проверить на переполнение."""
-        return self.size >= self.max_size
+        return self.__size >= self.__max_size
 
-    def push_back(self, item):
+    def push_back(self, value: int):
         """Добавить элемент в конец дека."""
         if self.overflow():
             raise OverfullError
-        self.queue[self.tail] = item
-        self.tail = (self.tail + 1) % self.max_size
-        self.size += 1
+        self.__deque_list[self.__back] = value
+        self.__back = self.calc_index(self.__back, 1)
+        self.__size += 1
 
-    def push_front(self, item):
+
+    def push_front(self, value: int):
         """Добавить элемент в начало дека."""
         if self.overflow():
             raise OverfullError
-        if self.head == 0 and self.tail == 0:
-            self.queue[self.head] = item
-            self.tail = (self.tail + 1) % self.max_size
-        elif self.head == 0 and self.tail == 1:
-            self.head = self.max_size - 1
-            self.queue[self.head] = item
-        else:
-            self.head = (self.head - 1) % self.max_size
-            self.queue[self.head] = item
-        self.size += 1
+        self.__deque_list[self.__front - 1] = value
+        self.__front = self.calc_index(self.__front, 0)
+        self.__size += 1
 
     def pop_front(self):
         """Вывести первый элемент дека и удалить его."""
         if self.is_empty():
             raise EmptyError
-        item = self.queue[self.head]
-        self.queue[self.head] = None
-        self.head = (self.head + 1) % self.max_size
-        self.size -= 1
-        return item
+        front = self.__deque_list[self.__front]
+        self.__deque_list[self.__front] = None
+        self.__front = self.calc_index(self.__front, 1)
+        self.__size -= 1
+        return front
 
     def pop_back(self):
         """Вывести последний элемент дека и удалить его."""
         if self.is_empty():
             raise EmptyError
-        if self.tail == 0:
-            self.tail = self.max_size - 1
-            item = self.queue[self.tail]
-            self.queue[self.tail] = None
-        else:
-            self.tail = (self.tail - 1) % self.max_size
-            item = self.queue[self.tail]
-            self.queue[self.tail] = None
-        self.size -= 1
-        return item
+        back = self.__deque_list[self.__back - 1]
+        self.__deque_list[self.__back - 1] = None
+        self.__back = self.calc_index(self.__back, 0)
+        self.__size -= 1
+        return back
 
 
 class EmptyError(Exception):
@@ -74,18 +67,26 @@ class OverfullError(Exception):
     pass
 
 
-if __name__ == '__main__':
+def handle_commands(command, deque):
+    """Команды."""
+    function, *args = command.split()
+    try:
+        return getattr(deque, function)(*args)
+    except AttributeError:
+        raise ValueError('Invalid value')
+    except (OverfullError, EmptyError):
+        return 'error'
+
+
+def main():
     number_commands = int(input())
-    max_size_deque = Deque(int(input()))
+    deque = Deque(int(input()))
     commands = [input() for _ in range(number_commands)]
-    result = []
     for command in commands:
-        function, *params = command.split()
-        try:
-            result = (getattr(max_size_deque, function)(*params))
-            if result:
-                print(result)
-        except OverfullError:
-            print('error')
-        except EmptyError:
-            print('error')
+        result = handle_commands(command, deque)
+        if result:
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
